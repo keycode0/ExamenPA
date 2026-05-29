f787318e553be604476e774d228841bf8879814c
keycode <keycode.master0@gmail.com>
2026-05-28 20:43:53 -0500

    correccion build-gradle

---
+++ src/main/java/com/estudiante/proyecto/examenedusmart/entity/CursoEntity.java
@@ -0,0 +1,55 @@
+package com.estudiante.proyecto.examenedusmart.entity;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tbl_cursos_base")
+public class CursoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_curso")
+    private Long idCurso;
+
+    @Column(name = "nombre_curso", length = 120, unique = true, nullable = false)
+    private String nombreCurso;
+
+    @Column(name = "precio_usd")
+    private BigDecimal precioUsd;
+
+    @Column(name = "horas_duracion")
+    private Integer horasDuracion;
+
+    public Long getIdCurso() {
+        return idCurso;
+    }
+
+    public void setIdCurso(Long idCurso) {
+        this.idCurso = idCurso;
+    }
+
+    public String getNombreCurso() {
+        return nombreCurso;
+    }
+
+    public void setNombreCurso(String nombreCurso) {
+        this.nombreCurso = nombreCurso;
+    }
+
+    public BigDecimal getPrecioUsd() {
+        return precioUsd;
+    }
+
+    public void setPrecioUsd(BigDecimal precioUsd) {
+        this.precioUsd = precioUsd;
+    }
+
+    public Integer getHorasDuracion() {
+        return horasDuracion;
+    }
+
+    public void setHorasDuracion(Integer horasDuracion) {
+        this.horasDuracion = horasDuracion;
+    }
+}
