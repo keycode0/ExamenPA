69c380c9b3ca9f8da48bab3ae1d69fdb72e4f1fb
keycode <keycode.master0@gmail.com>
2026-05-28 20:36:40 -0500

    configurar perfil prod y db edusmart

---
+++ src/main/java/com/estudiante/proyecto/examenedusmart/ExamenEduSmartApplication.java
@@ -0,0 +1,13 @@
+package com.estudiante.proyecto.examenedusmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenEduSmartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenEduSmartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/estudiante/proyecto/examenedusmart/ExamenEduSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudiante.proyecto.examenedusmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenEduSmartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
